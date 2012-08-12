6159e2d0423084f2a7626e58fa8b68dd30c60460
Guilhem Vellut <guilhem.vellut@gmail.com>
2012-08-11 20:03:55 -0700

    - Corrected the deltaIP change operators - Added a default operand (Integer[0])

--- src/com/vellut/fungeworld/Interpreter.java
+++ src/com/vellut/fungeworld/Interpreter.java
@@ -10,6 +10,13 @@ public class Interpreter {
 	private InterpreterState state;
 	private Stack<Instruction> executionStack;
 
+	private static final Instruction DEFAULT_OPERAND;
+
+	static {
+		DEFAULT_OPERAND = new Instruction(InstructionType.INTEGER);
+		DEFAULT_OPERAND.setAttachedData(Integer.valueOf(0));
+	}
+
 	public Interpreter(int dimension, MemoryReaderWriter memoryReaderWriter) {
 		this.dimension = dimension;
 		this.memoryReaderWriter = memoryReaderWriter;
@@ -51,126 +58,124 @@ public class Interpreter {
 
 			switch (instr.getInstructionType()) {
 
-			case DUP:
+			case DUP: {
 				// Duplicate top element of the stack
-				if(!executionStack.empty()) {
-					executionStack.push(executionStack.peek());
-				} // else like noop
+				// If stack is empty: two '0' are pushed
+				Instruction op = popOperand();
+				pushInstruction(op);
+				pushInstruction(op);
+			}
 				break;
-			case SWAP:
+			case SWAP: {
 				// Swap top 2 elements of the stack
-				if(executionStack.size() >= 2) {
-					Instruction instr1 = executionStack.pop();
-					Instruction instr2 = executionStack.pop();
-					executionStack.push(instr1);
-					executionStack.push(instr2);
-				} // else like noop
+				// If stack is empty: two '0' are pushed
+				Instruction instr1 = popOperand();
+				Instruction instr2 = popOperand();
+				pushInstruction(instr1);
+				pushInstruction(instr2);
+			}
 				break;
 			case POP:
-				if(!executionStack.empty()) {
-					executionStack.pop();
-				} // else like noop
+				popOperand(); // ignore
 				break;
 			case BRIDGE:
 				// Jumps IP one more
 				incrementInstructionPointer();
 				break;
-			case ADD:
-				if(executionStack.size() >= 2) {
-					int op1 = getIntegerOperand();
-					int op2 = getIntegerOperand();
-					pushInteger(op1 + op2);
-				} // else like noop
+			case ADD: {
+				// Default value is 0 in case stack empty or bad type
+				int op1 = popIntegerOperand();
+				int op2 = popIntegerOperand();
+				pushInteger(op1 + op2);
+			}
 				break;
-			case SUB:
-				if(executionStack.size() >= 2) {
-					int op1 = getIntegerOperand();
-					int op2 = getIntegerOperand();
-					pushInteger(op2 - op1);
-				} // else like noop
+			case SUB: {
+				int op1 = popIntegerOperand();
+				int op2 = popIntegerOperand();
+				pushInteger(op2 - op1);
+			}
 				break;
-			case DIV:
-				if(executionStack.size() >= 2) {
-					int op1 = getIntegerOperand();
-					int op2 = getIntegerOperand();
+			case DIV: {
+				int op1 = popIntegerOperand();
+				int op2 = popIntegerOperand();
+				if (op1 != 0) {
 					pushInteger(op2 / op1);
-				} // else like noop
+				} else {
+					if (op2 == 0) {
+						pushInteger(1);
+					} else {
+						pushInteger(0);
+					}
+				}
+			}
 				break;
-			case MUL:
-				if(executionStack.size() >= 2) {
-					int op1 = getIntegerOperand();
-					int op2 = getIntegerOperand();
-					pushInteger(op2 * op1);
-				} // else like noop
+			case MUL: {
+				int op1 = popIntegerOperand();
+				int op2 = popIntegerOperand();
+				pushInteger(op2 * op1);
+			}
 				break;
-			case EQ:
-				if(executionStack.size() >= 2) {
-					// Can be performed on more than just integers
-					// (although int version 1 it is all there is)
-					Instruction instr1 = executionStack.pop();
-					Instruction instr2 = executionStack.pop();
-					boolean eq = instr1.equals(instr2);
-					pushInteger(eq?1:0);
-				} // else like noop
+			case EQ: {
+				// Can be performed on more than just integers
+				// (although int version 1 it is all there is)
+				Instruction instr1 = popOperand();
+				Instruction instr2 = popOperand();
+				boolean eq = instr1.equals(instr2);
+				pushInteger(eq ? 1 : 0);
+			}
 				break;
-			case GT:
-				if(executionStack.size() >= 2) {
-					int op1 = getIntegerOperand();
-					int op2 = getIntegerOperand();
-					pushInteger(op2 > op1?1:0);
-				} // else like noop
+			case GT: {
+				int op1 = popIntegerOperand();
+				int op2 = popIntegerOperand();
+				pushInteger(op2 > op1 ? 1 : 0);
+			}
 				break;
-			case NOT:
-				if(!executionStack.empty()) {
-					int op1 = getIntegerOperand();
-					pushInteger(op1 == 0?1:0);
-				} // else like noop
+			case NOT: {
+				int op1 = popIntegerOperand();
+				pushInteger(op1 == 0 ? 1 : 0);
+			}
 				break;
 			case INTEGER:
 				executionStack.push(instr);
 				break;
 			case ABS_DPOS_INCR_0:
-				updateInstructionPointer(0, 1);
+				updateDeltaInstructionPointer(0, 1);
 				break;
 			case ABS_DPOS_DECR_0:
-				updateInstructionPointer(0, -1);
+				updateDeltaInstructionPointer(0, -1);
 				break;
 			case ABS_DPOS_INCR_1:
-				updateInstructionPointer(1, 1);
+				updateDeltaInstructionPointer(1, 1);
 				break;
 			case ABS_DPOS_DECR_1:
-				updateInstructionPointer(1, -1);
+				updateDeltaInstructionPointer(1, -1);
 				break;
-			case ABS_DPOS_COND_0:
-				if(!executionStack.empty()) {
-					int op1 = getIntegerOperand();
-					updateInstructionPointer(0, op1 == 0?1:-1);
-				} // else like noop
+			case ABS_DPOS_COND_0: {
+				int op1 = popIntegerOperand();
+				updateDeltaInstructionPointer(0, op1 == 0 ? 1 : -1);
+			}
 				break;
-			case ABS_DPOS_COND_1:
-				if(!executionStack.empty()) {
-					int op1 = getIntegerOperand();
-					updateInstructionPointer(1, op1 == 0?1:-1);
-				} // else like noop
+			case ABS_DPOS_COND_1: {
+				int op1 = popIntegerOperand();
+				updateDeltaInstructionPointer(1, op1 == 0 ? 1 : -1);
+			}
 				break;
 			case END:
 				this.state = InterpreterState.KILLED;
 				break;
-			case READ:
-				if(!executionStack.empty()) {
-					int y = getIntegerOperand();
-					int x = getIntegerOperand();
-					this.state = InterpreterState.EXPECTING_READ_RESPONSE;
-					memoryReaderWriter.read(new int[]{x,y});
-				}
+			case READ: {
+				int y = popIntegerOperand();
+				int x = popIntegerOperand();
+				this.state = InterpreterState.EXPECTING_READ_RESPONSE;
+				memoryReaderWriter.read(new int[] { x, y });
+			}
 				break;
-			case WRITE:
-				if(!executionStack.empty()) {
-					int y = getIntegerOperand();
-					int x = getIntegerOperand();
-					Instruction instrValue = executionStack.pop();
-					memoryReaderWriter.write(new int[]{x,y}, instrValue);
-				}
+			case WRITE: {
+				int y = popIntegerOperand();
+				int x = popIntegerOperand();
+				Instruction instrValue = popOperand();
+				memoryReaderWriter.write(new int[] { x, y }, instrValue);
+			}
 				break;
 			default:
 
@@ -188,11 +193,18 @@ public class Interpreter {
 		}
 	}
 	
-	private void updateInstructionPointer(int dimension, int deltaValue) {
-		instructionPointer[dimension] += deltaValue;
+	private void updateDeltaInstructionPointer(int dimension, int deltaValue) {
+		for (int i = 0; i < deltaInstructionPointer.length; i++) {
+			deltaInstructionPointer[i] = 0;
+		}
+		deltaInstructionPointer[dimension] = deltaValue;
 	}
 	
-	private int getIntegerOperand() {
+	private int popIntegerOperand() {
+		if (executionStack.empty()) {
+			return 0;
+		}
+
 		Instruction instr1 = executionStack.pop();
 		// 0 is the default values in case the value on the stack
 		// is not an integer
@@ -203,10 +215,21 @@ public class Interpreter {
 		return op1;
 	}
 	
+	private Instruction popOperand() {
+		if (executionStack.empty()) {
+			return DEFAULT_OPERAND;
+		}
+		return executionStack.pop();
+	}
+
 	private void pushInteger(int value) {
 		Instruction result = new Instruction(InstructionType.INTEGER);
 		result.setAttachedData(value);
 		executionStack.push(result);
+	}
+
+	private void pushInstruction(Instruction instr) {
+		executionStack.push(instr);
 	}
 
 	// When read response received, this method must be called by the
