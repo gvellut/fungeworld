1791d4a2372ae837b6c1d198bd0b675533645d40
Guilhem Vellut <guilhem.vellut@gmail.com>
2012-09-09 22:12:34 +0200

    Added test with more complex execution (outputs itself)

--- test/com/vellut/fungeword/simple/FungeWorldBoardTest.java
+++ test/com/vellut/fungeword/simple/FungeWorldBoardTest.java
@@ -3,6 +3,8 @@ package com.vellut.fungeword.simple;
 import static org.junit.Assert.assertEquals;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -30,11 +32,47 @@ public class FungeWorldBoardTest {
 		// The program simple.fw outputs a 3 at coordinates [1,0]
 		fw.run(new int[] { 0, 0 }, new int[] { 1, 0 });
 
-		System.out.println(fw.toString());
-
 		Instruction output = board[0][1];
 		assertEquals(InstructionType.INTEGER, output.getInstructionType());
 		assertEquals(Integer.valueOf(3), (Integer) output.getAttachedData());
+	}
+
+	@Test
+	public void testTroll1Execution() throws Exception {
+		FungeWorldBoard fw = new FungeWorldBoard(100, 100);
+		ProgramReader reader = new ProgramReader();
+		// The program outputs itself
+		// see http://www.nyx.net/~gthompso/self_bf.txt
+		InputStream is = this.getClass().getClassLoader()
+				.getResourceAsStream("data/troll1.fw");
+		Instruction[][] program = reader.readProgram(is);
+		fw.load(program, new int[] { 0, 0 });
+
+		fw.run(new int[] { 0, 0 }, new int[] { 1, 0 });
+
+		List<Instruction> response = fw.getOutput();
+
+		// What the output should be : The source of the program output
+		// with i10 between each line
+		List<Instruction> shouldBe = new ArrayList<>();
+		// Line by line
+		for (int j = 0; j < program[0].length; j++) {
+			for (int i = 0; i < program.length; i++) {
+				shouldBe.add(program[i][j]);
+			}
+			// The original troll does I/O slightly differently
+			// and outputs the ASCII value 10 (newline) between
+			// each line. However we don't do any conversion to/from ASCII
+			Instruction instr10 = new Instruction(InstructionType.INTEGER);
+			instr10.setAttachedData(10);
+			shouldBe.add(instr10);
+		}
+
+		assertEquals(shouldBe.size(), response.size());
+		for (int i = 0; i < response.size(); i++) {
+			assertEquals(shouldBe.get(i), response.get(i));
+		}
+
 	}
 
 }
